156df240a0ca59a17456cae70f345ad3aa94f7f8
Harini Srinivasan <harini7201@gmail.com>
2026-03-29 18:12:05 +0530

    Delete sql directory

--- sql/revenue_trends.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-DATE_FORMAT(order_date, '%Y-%m') AS Month,
-COUNT(order_id) AS Total_Orders,
-SUM(quantity) AS Books_Sold,
-ROUND(SUM(total_amount), 2) AS Monthly_Revenue
-FROM orders
-GROUP BY DATE_FORMAT(order_date, '%Y-%m')
-ORDER BY Month;
